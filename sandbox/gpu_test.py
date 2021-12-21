853751971df678f929029cc24b424cc219e10987
Kevin Lybarger <lybarger@uw.edu>
2021-12-21 10:51:07 -0800

    added BRAT configuration files

---
+++ sandbox/gpu_test.py
@@ -0,0 +1,21 @@
+import torch
+
+n = 1000
+m = 1000
+size = (n, n, n)
+
+x = torch.rand(size, device=0)
+# x = x.to(0)
+
+print('x', x.shape, x.device)
+print('start')
+
+for i in range(m):
+
+    a = x*x
+    z = a.sum()
+
+    if i % 100 == 0:
+        print(f'{i}/{m}', z)
+
+print('end', i)
